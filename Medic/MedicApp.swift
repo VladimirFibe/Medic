40482ed94631b82e22a88d056aa73f12211645d8
Vladimir <motiw@icloud.com>
2023-03-03 21:12:44 +0600

    Initial Commit

---
+++ Medic/MedicApp.swift
@@ -0,0 +1,17 @@
+//
+//  MedicApp.swift
+//  Medic
+//
+//  Created by Vladimir on 03.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct MedicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
