ea133cab37563314ae2adbe2f97859ddeb5b4f62
Vladimir <motiw@icloud.com>
2023-04-05 15:33:11 +0600

    Basket

--- Medic/Feature/Basket/BasketView.swift
+++ Medic/Feature/Basket/BasketView.swift
@@ -9,10 +9,51 @@ import SwiftUI
 
 struct BasketView: View {
     var body: some View {
-        Text("Корзина")
+        VStack {
+            HStack {
+                Text("Корзина")
+                Spacer()
+                Image(systemName: "trash")
+            }
+            List(0 ..< 2) { item in
+                ForEach(0 ..< 5) { item in
+                    BasketItem()
+                }
+            }
+            .listStyle(.plain)
+            .listRowSeparator(.hidden)
+        }
+        .padding()
     }
 }
 
+struct BasketItem: View {
+    @State private var step = 1
+    var body: some View {
+        VStack {
+            HStack {
+                Text("Клинический анализ крови с лейкоцитарной формулировкой")
+                Spacer()
+                Image(systemName: "xmark")
+            }
+            Spacer()
+            HStack {
+                Text("690 ₽")
+                Spacer()
+                Stepper(value: $step, in: 1...9) {
+                    Text("\(step) пациент")
+                }
+                .frame(width: 200)
+            }
+            
+        }
+        .padding(8)
+        .frame(height: 138)
+        .background(Color.white)
+        .cornerRadius(10)
+        .shadow(radius: 1)
+    }
+}
 struct BasketView_Previews: PreviewProvider {
     static var previews: some View {
         BasketView()
